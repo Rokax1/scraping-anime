1057a1866520e461197e54c716c3a30b5a12594d
leandro <leandro@nublelabs.cl>
2024-03-24 21:55:43 -0300

    fix: get ulrs test 1 anime

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
-const { chromium } = require("@playwright/test");
-import { storeAnime } from './store';
+import { chromium } from "@playwright/test";
 
 (async () => {
-  console.log(store.getConection(),'store');
   const selector = "ul.ListAnimes li article.Anime a";
   const selectorAnimePage = "ul.ListCaps > li";
   const url = "https://www3.animeflv.net";
@@ -11,6 +9,7 @@ import { storeAnime } from './store';
   const context = await browser.newContext();
   const page = await context.newPage();
   // Set custom headers
+
   await page.setExtraHTTPHeaders({
     "User-Agent":
       "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
@@ -25,17 +24,16 @@ import { storeAnime } from './store';
   let linksAnimeArray = [...uniqueLinks];
 
   //TODO: borrar esta linea para obtenerlos todos
-  //linksAnimeArray = linksAnimeArray.slice(7, 9);
+  linksAnimeArray = linksAnimeArray.slice(0, 1);
 
   linksAnimeArray = linksAnimeArray.map((link) => {
     return getShowCapAnimeUrl(link, url);
   });
 
-  //console.log(linksAnimeArray, linksAnimeArray.length);
-  // await page.goto(linksAnimeArray[15]);
+
 
   //TODO: falta hacer un foreach por cada anime
-
+  console.log(linksAnimeArray);
   let linksShowAnime = [];
   for (const url of linksAnimeArray) {
     await page.goto(url);
@@ -70,37 +68,45 @@ import { storeAnime } from './store';
       isAnime: isAnimeValue,
       isFinished: isFinishedValue,
     });
+
   }
 
   console.log(linksShowAnime);
 
-  let i=0;
+  return;
   for (let anime of linksShowAnime) {
-
     for (let url_view_anime of anime.urls) {
       await page.goto(url_view_anime);
 
-      await page.waitForLoadState("networkidle");
-      const aElement = await page.$('li[data-id="1"] a');
-      await aElement.click();
-      if(i==0){
-        await page.waitForTimeout(1000);
-        const popupPromise = page.waitForEvent("popup");
-        const popup = await popupPromise;
-        popup.close();
-      }
+      // const aElement = await page.$('li[data-id="1"] a');
+      // const htmlContent = await page.evaluate(element => element.innerHTML, aElement);
+      // console.log(htmlContent);
+      // await aElement.click();
 
- 
-      // Esperar unos segundos
-      await page.waitForLoadState("networkidle");
 
-      await aElement.click();
+      // const iframeElement = await page.$("div#video_box iframe");
+      // const iframeSrc = await iframeElement.evaluate((iframe) => iframe.src);
 
-      const iframeElement = await page.$("div#video_box iframe");
-      const iframeSrc = await iframeElement.evaluate((iframe) => iframe.src);
+      // console.log(iframeSrc, url_view_anime);
 
-      console.log(iframeSrc, url_view_anime);
-      i++;
+      const options = await page.$$('.CapiTnv li'); 
+      const htmlContent = await page.evaluate(element => element.innerHTML, options); 
+    for (const option of options) {
+        const aElement = await option.$('a'); 
+        
+        console.log(htmlContent); 
+
+        await aElement.click(); 
+
+        const iframeElement = await page.$("div#video_box iframe"); 
+        let iframeSrc=null;
+        if (iframeElement) {
+           iframeSrc = await iframeElement.evaluate((iframe) => iframe.src); 
+        }
+
+        console.log(iframeSrc, url_view_anime); 
+    }
+
     }
   }
   await browser.close();
